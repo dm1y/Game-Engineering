30e6f1f6cec0ede38e806ab70a9eb5a68afff9bc
dm1y <dianamly@live.com>
2014-09-24 21:38:50 -0700

    Added Project2, contains the different switch screens. [Basic layout]

---
+++ Project2/Project2/Project2/StartScreen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Project2
+{
+    public class StartScreen
+    {
+        private Game1 game;
+        private KeyboardState lastState;
+        private Texture2D texture; /* Place holder if you want to have a picture for the start game screen */
+        private SpriteFont font; /* Place holder if you want to have text display instructions */
+
+        public StartScreen(Game1 game)
+        {
+            this.game = game;
+            lastState = Keyboard.GetState();
+            //texture = game.Content.Load<Texture2D>("");
+            font = game.Content.Load<SpriteFont>("SpriteFont1");
+        }
+
+        public void Update()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            /* Starts the game */
+            if (keyboardState.IsKeyDown(Keys.Enter) && lastState.IsKeyUp(Keys.Enter))
+            {
+                game.StartGame();
+            }
+            /* Exits the game */
+            else if (keyboardState.IsKeyDown(Keys.Escape) && lastState.IsKeyUp(Keys.Escape))
+            {
+                game.Exit();
+            }
+
+            lastState = keyboardState;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (texture != null)
+                spriteBatch.Draw(texture, new Vector2(0f, 0f), Color.White);
+
+            // Used as placeholders 
+
+            // Used if you want to draw the instructions on 
+            spriteBatch.DrawString(font, "START SCREEN ",
+               new Vector2(game.GraphicsDevice.Viewport.Width / 3 + 50, game.GraphicsDevice.Viewport.Height/3),
+               Color.Black); 
+
+            // Used if you want to draw the instructions on 
+             spriteBatch.DrawString(font, "PRESS [ENTER] TO START \n(Warning: does not do anything) \n PRESS [ESC] TO EXIT",
+                new Vector2(game.GraphicsDevice.Viewport.Width / 3 - 7, game.GraphicsDevice.Viewport.Height - 100),
+                Color.GhostWhite); 
+        }
+    }
+}
---
+++ Project2/Project2/Project2/World.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Project2
+{
+    public class World
+    {
+        public World(Game1 game)
+        { 
+            // Do stuff 
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            // Do stuff 
+        }
+
+        public void Update(GameTime gametime)
+        { 
+            // Do stuff 
+        }
+
+        public void Draw(SpriteBatch sb)
+        { 
+            // Do stuff
+        }
+
+    }
+}
